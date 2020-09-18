47625ac8d04fcea0ea29acb917ad8b4ca3415818
concrode <captainshenhua@gmail.com>
2020-09-18 16:18:03 +1000

    Updates

---
+++ tomcat_8.5_source_project/servlet_demo01/src/cn/yuntian/web/BbsServlet.java
@@ -0,0 +1,20 @@
+package cn.yuntian.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BbsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("---------------- >> Get : BbsServlet find all ......");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("---------------- >> Post : BbsServlet find all ......");
+    }
+}
---
+++ tomcat_8.5_source_project/servlet_demo01/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jack
+  Date: 18/9/20
+  Time: 4:01 pm
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
