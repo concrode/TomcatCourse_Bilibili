1d98f3ee9d796ee683d2484a540429b516c3ecfb
concrode <captainshenhua@gmail.com>
2020-09-19 23:39:50 +1000

    Updates

---
+++ tomcat_project/servlet_demo01/src/cn/he/web/BbsServlet.java
@@ -0,0 +1,23 @@
+package cn.he.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by captain on 19/9/20.
+ */
+public class BbsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("------------- > Get: BbsServlet find all .....");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("------------- > Post: BbsServlet find all .....");
+    }
+}
