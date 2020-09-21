391ff9e4887a402ae72e95c4cbd42cc8dc637608
concrode <captainshenhua@gmail.com>
2020-09-21 22:06:36 +1000

    updates

---
+++ tomcat_project/servlet_demo01/src/cn/he/com/Bbsservlet.java
@@ -0,0 +1,20 @@
+package cn.he.com;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Bbsservlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("************ >> Bbsservlet doGet ....");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("************ >> Bbsservlet doPost ....");
+    }
+}
