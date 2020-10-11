431b6130fc8668283c547e8acef6c056433bc1c9
concrode <captainshenhua@gmail.com>
2020-10-11 16:45:19 +1100

    Updates

--- tomcat_project/servlet_demo01/src/cn/he/com/Bbsservlet.java
+++ tomcat_project/servlet_demo01/src/cn/he/com/Bbsservlet.java
@@ -11,6 +11,15 @@ public class Bbsservlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("************ >> Bbsservlet doGet ....");
+
+        String value = req.getServletContext().getInitParameter("param_01");
+        System.out.println(value);
+
+        String id = req.getSession().getId();
+        System.out.println("SessionId: " + id);
+
+        int i = 1 / 0;
+
     }
 
     @Override
